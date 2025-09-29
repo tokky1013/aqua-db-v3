f6fb59d2bbc00cb33298dcc5e3957d9a90a66fbd
tokky1013 <a.toshiaki1013@gmail.com>
2025-09-30 00:00:12 +0900

    履歴を表示

--- js/funcs.js
+++ js/funcs.js
@@ -68,7 +68,7 @@ function showSongs(songArr) {
         html += `
             <label onclick="displaySongDetail('${song.uuid}')">
                 <h4>${song.title}</h4>
-                <p>${song.artist}　${song.getCreatedAt()}</p>
+                <div><div>${song.artist}</div> <div><span>追加日</span> : ${song.getCreatedAt()}</div></div>
                 <table>
                     <tr>
                         <th>地低</th>
@@ -93,8 +93,47 @@ function showSongs(songArr) {
 }
 
 function showHistories(historyArr) {
+    let html = '';
+    historyArr.forEach((history) => {
+        let key = history.key;
+        if(key > 0) {
+            key = 'キー +' + key;
+        } else if(key === 0) {
+            key = '原曲キー';
+        } else {
+            key = 'キー ' + key;
+        }
+
+        html += `
+            <label onclick="displayHistoryDetail('${history.uuid}')">
+                <h4>${history.song.title}</h4>
+                <div><div>${key}</div><div>${history.song.artist}</div><div>${history.getCreatedAt()}</div></div>
+                <table>
+                    <tr>
+                        <th>地低</th>
+                        <th>地高</th>
+                        <th>裏低</th>
+                        <th>裏高</th>
+                        <th>最高音</th>
+                    </tr>
+                    <tr>
+                        <td>${history.getChestMinNote()}</td>
+                        <td>${history.getChestMaxNote()}</td>
+                        <td>${history.getHeadMinNote()}</td>
+                        <td>${history.getHeadMaxNote()}</td>
+                        <td>${history.getOverallMaxNote()}</td>
+                    </tr>
+                </table>
+            </label>
+        `
+    });
+
+    $('#history-list').html(html);
 }
 
 function displaySongDetail(uuid) {
+    alert(uuid);
+}
+function displayHistoryDetail(uuid) {
     alert(uuid);
 }
